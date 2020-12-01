88b0ee7b35a478aed73d67896d1783772bd25cc8
lenya <66315146+Lev0nid@users.noreply.github.com>
2020-12-01 23:32:00 +0300

    Решение задачи

---
+++ src/sortings.c
@@ -0,0 +1,148 @@
+#include "sortings.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
+static void swap(char** a, char** b) {
+    char* tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+static int partition(strings_array_t strings, size_t left, size_t right, comparator_func_t cmp) {
+    char* middle_string = strings[(left + right) / 2];
+    while(left <= right) {
+        while(cmp(strings[left], middle_string) > 0)
+            left++;
+        while(cmp(middle_string, strings[right]) > 0)
+            right--;
+        if(left >= right) break;
+        swap(&strings[left], &strings[right]);
+        left++;
+        right--;
+    }
+    return right;
+}
+
+static void quick_sort_internal(strings_array_t strings, size_t left, size_t right, comparator_func_t cmp) {
+    if(left < right) {
+        size_t mid = partition(strings, left, right, cmp);
+        quick_sort_internal(strings, left, mid, cmp);
+        quick_sort_internal(strings, mid + 1, right, cmp);
+    }
+}
+
+void quick(strings_array_t strings, array_size_t amount_of_strings, comparator_func_t cmp) {
+    quick_sort_internal(strings, 0, amount_of_strings - 1, cmp);
+}
+
+void bubble(strings_array_t strings, array_size_t amount_of_strings, comparator_func_t cmp) {
+    for(size_t i = 0; i < amount_of_strings; i++) {
+        for(size_t j = i + 1; j < amount_of_strings; j++) {
+            if(cmp(strings[i], strings[j]) <= 0)
+                swap(&strings[i], &strings[j]);
+        }
+    }
+}
+
+void insertion(strings_array_t strings, array_size_t amount_of_strings, comparator_func_t cmp) {
+    for(size_t i = 0; i < amount_of_strings; i++) {
+        for(size_t j = i; j > 0 && cmp(strings[j - 1], strings[j]) <= 0; j--)
+            swap(&strings[j-1], &strings[j]);
+    }
+}
+
+static void merge_arrays(strings_array_t strings, strings_array_t buffer, size_t left,
+                         size_t middle, size_t right, comparator_func_t cmp) {
+    size_t i1 = 0, i2 = 0;
+
+    while(left + i1 < middle && middle + i2 < right) {
+        if(cmp(strings[left + i1], strings[middle + i2]) > 0) {
+            buffer[i1 + i2] = strings[left + i1];
+            i1++;
+        } else {
+            buffer[i1 + i2] = strings[middle + i2];
+            i2++;
+        }
+    }
+
+    for(; left + i1 < middle; i1++)
+        buffer[i1 + i2] = strings[left + i1];
+
+    for(; middle + i2 < right; i2++)
+        buffer[i1 + i2] = strings[middle + i2];
+
+    for(int i = 0; i < i1 + i2; i++)
+        strings[left + i] = buffer[i];
+}
+
+void merge(strings_array_t strings, array_size_t amount_of_strings, comparator_func_t cmp) {
+    strings_array_t buffer = malloc(amount_of_strings * sizeof(char*));
+    if (buffer == NULL) {
+        fprintf(stderr, "Merge sort buffer memory allocation error\n");
+        free(buffer);
+        exit(-1);
+    }
+    for(size_t i = 1; i < amount_of_strings; i *= 2) {
+        for(size_t j = 0; j < amount_of_strings - i; j += i * 2) {
+            merge_arrays(strings, buffer, j, j + i, min(j + 2 * i, amount_of_strings), cmp);
+        }
+    }
+    free(buffer);
+}
+
+static void radix_sort_internal(strings_array_t strings, array_size_t amount_of_strings,
+                      size_t left, size_t right, size_t position){
+    if(position >= MAX_INPUT_STRING_SIZE || left + 1 >= right) return;
+
+    int count[UCHAR_MAX + 1];
+    for(size_t i = 0; i < UCHAR_MAX + 1; i++){
+        count[i] = 0;
+    }
+
+    for(size_t i = left; i < right; i++){
+        count[(unsigned char)strings[i][position]]++;
+    }
+    for(size_t i = 1; i < UCHAR_MAX + 1; i++){
+        count[i] += count[i - 1];
+    }
+    int count_copy[UCHAR_MAX + 1];
+    memcpy(count_copy, count, (UCHAR_MAX + 1)*sizeof(int));
+
+    strings_array_t bucket = malloc((right - left) * sizeof(char*));
+    if(bucket == NULL) {
+        fprintf(stderr, "Radix sort bucket memory allocation error\n");
+        free(bucket);
+        exit(-1);
+    }
+
+    for(size_t i = left; i < right; i++) {
+        bucket[--count[(unsigned char) strings[i][position]]] = strings[i];
+    }
+    memcpy(strings + left, bucket, (right - left)*sizeof(char*));
+
+    radix_sort_internal(strings, amount_of_strings, left, left + count_copy[0], position + 1);
+    for(int i = 1; i < UCHAR_MAX + 1; i++){
+        radix_sort_internal(strings, amount_of_strings, left + count_copy[i - 1],
+                            left + count_copy[i], position + 1);
+    }
+
+    free(bucket);
+}
+
+
+void radix(strings_array_t string, array_size_t amount_of_strings, comparator_func_t cmp){
+    radix_sort_internal(string, amount_of_strings, 0, amount_of_strings, 0);
+    if(cmp("1", "2") < 0) {
+        for(size_t i = 0; i < amount_of_strings / 2; i++) {
+            swap(&string[i], &string[amount_of_strings - i - 1]);
+        }
+    }
+}
---
+++ src/strings_comparer.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <string.h>
+#include "sortings.h"
+
+int compare_strings_ascending(const char* string1, const char* string2) {
+    return strcmp(string2, string1);
+}
+
+int compare_strings_descending(const char* string1, const char* string2) {
+    return strcmp(string1, string2);
+}
+
+int parse_params(char** argv, array_size_t* amount_of_strings, comparator_func_t* cmp) {
+    char* end_pointer;
+    *amount_of_strings = strtoul(argv[1], &end_pointer, 10);
+    if(*end_pointer != '\0' || *amount_of_strings <= 0) {
+        fprintf(stderr, "Second param is not a positive integer");
+        return -1;
+    }
+
+    static const char* asc = "asc";
+    static const char* des = "des";
+
+    if(strcmp(asc, argv[5]) == 0) {
+        *cmp = compare_strings_ascending;
+    } else if(strcmp(des, argv[5]) == 0) {
+        *cmp = compare_strings_descending;
+    } else {
+        fprintf(stderr, "Unsupported comparator");
+        return -1;
+    }
+    return 0;
+}
+
+int sort_strings(char** argv, array_size_t amount_of_strings, strings_array_t strings, comparator_func_t cmp) {
+    static const char* word_bubble = "bubble";
+    static const char* word_merge = "merge";
+    static const char* word_quick = "quick";
+    static const char* word_insertion = "insertion";
+    static const char* word_radix = "radix";
+
+    if(strcmp(word_bubble, argv[4]) == 0) {
+        bubble(strings, amount_of_strings, cmp);
+    } else if(strcmp(word_merge, argv[4]) == 0) {
+        merge(strings, amount_of_strings, cmp);
+    } else if(strcmp(word_quick, argv[4]) == 0) {
+        quick(strings, amount_of_strings, cmp);
+    } else if(strcmp(word_insertion, argv[4]) == 0) {
+        insertion(strings, amount_of_strings, cmp);
+    } else if(strcmp(word_radix, argv[4]) == 0) {
+        radix(strings, amount_of_strings, cmp);
+    } else {
+        fprintf(stderr, "Unsupported sorting algorithm");
+        return -1;
+    }
+    return 0;
+}
+
+int read_strings_from_file(FILE* file, array_size_t amount_of_strings, strings_array_t strings) {
+    size_t i;
+    for(i = 0; i < amount_of_strings && !feof(file); i++){
+        if(fgets(strings[i], MAX_INPUT_STRING_SIZE, file) == NULL) {
+            fprintf(stderr, "Reading string #%zu error", i + 1);
+            return -1;
+        }
+    }
+    strncat(strings[i-1], "\n", 1);
+    if(i < amount_of_strings) {
+        fprintf(stderr, "Not enough strings in file");
+        return -1;
+    }
+    if(!feof(file)) {
+        fprintf(stderr, "Too many strings in file");
+        return -1;
+    }
+    return 0;
+}
+
+int put_strings_in_file(FILE* file, array_size_t amount_of_strings, strings_array_t strings) {
+    for(size_t i = 0; i < amount_of_strings; i++) {
+        if(fputs(strings[i], file) == EOF) {
+            fprintf(stderr, "Printing string #zu error", (i + 1));
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    if(argc != 6) {
+        fprintf(stderr, "Incorrect amount of params");
+        return -1;
+    }
+    array_size_t amount_of_strings;
+    comparator_func_t cmp = NULL;
+    FILE* input_file;
+    FILE* output_file;
+
+    if(parse_params(argv, &amount_of_strings, &cmp) == -1) {
+        return -1;
+    }
+
+    input_file = fopen(argv[2], "r");
+    if(input_file == NULL) {
+        fclose(input_file);
+        fprintf(stderr, "Could not open %s file", argv[2]);
+        return -1;
+    }
+
+    strings_array_t strings = malloc(amount_of_strings * sizeof(char*));
+    if (strings == NULL) {
+        fprintf(stderr, "Strings array memory allocation error\n");
+        return -1;
+    }
+    for(size_t i = 0; i < amount_of_strings; i++) {
+        strings[i] = (char*)malloc(sizeof(char)*(MAX_INPUT_STRING_SIZE + 1));
+        if(strings[i] == NULL) {
+            fprintf(stderr, "String #%zu of strings array memory allocation error\n", i + 1);
+            for(int j = 0; j < i; j++)
+                free(strings[j]);
+            free(strings);
+            return -1;
+        }
+    }
+
+    if(read_strings_from_file(input_file, amount_of_strings, strings) == -1) {
+        for(size_t i = 0; i < amount_of_strings; i++)
+            free(strings[i]);
+        free(strings);
+        fclose(input_file);
+        return -1;
+    }
+    fclose(input_file);
+
+    if(sort_strings(argv, amount_of_strings, strings, cmp) == -1) {
+        return -1;
+    }
+
+    output_file = fopen(argv[3], "w");
+    if(output_file == NULL) {
+        fclose(output_file);
+        fprintf(stderr, "Could not open %s file", argv[3]);
+        return -1;
+    }
+
+    if(put_strings_in_file(output_file, amount_of_strings, strings) == -1) {
+        for(size_t i = 0; i < amount_of_strings; i++)
+            free(strings[i]);
+        free(strings);
+        fclose(output_file);
+        return -1;
+    }
+
+    for (size_t i = 0; i < amount_of_strings; i++)
+        free(strings[i]);
+    free(strings);
+    fclose(output_file);
+    return 0;
+}
